536a030858ba314bc59fbc94177b4bf7557a6625
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-11 21:24:40 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,16 +1,17 @@
-using_times ="Windgardium Leviosa"
+def using_times("Windgardium Leviosa")
 
 7.times do
   puts " #{using_times}"
   # using_times == looping_string
 end
 
-1.	counter = 0
-2.	 
-3.	loop do 
-4.	  counter += 1
-5.	  puts "Iteration #{counter} of the loop"
-6.	  if counter >= 10 
-7.	    break
-8.	  end
-9.	end
+counter = 0
+	 
+	loop do 
+	  counter += 1
+	  puts " #{using_times} #{counter}"
+	  if counter >= 7 
+	    break
+	  end
+  end
+end
