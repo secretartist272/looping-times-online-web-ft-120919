7d58489b17cb06e2ae706f04133b2556aec2dbb2
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-11 20:03:10 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,11 @@
-def using_times
-	#your code here
+using_times ="Windgardium Leviosa"
+
+7.times do
+  puts " #{using_times}"
+  # using_times == looping_string
 end
 
-
-
-
+7.times do
+  # looping_string = using_times
+  puts "#{looping_string}"
+end
